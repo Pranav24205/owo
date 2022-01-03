353672fb1b9048e93df398ccdb3b7e274ef12fff
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-03 18:57:30 +0530

    Update Config.py

--- src/Config.py
+++ src/Config.py
@@ -1,12 +1,13 @@
 import os
 
 class Config:
-    aid = int(os.environ.get("18622297"))
-    ahash = os.environ.get("27e6993af0786f66f96599db6cd10bcc")
-    bot_token = os.environ.get("5006835603:AAEOu5gqL7vi6CxnxLCo4lCgxh95aWFH2fU")
+
+    aid = int(os.environ.get("APP_ID", "18622297"))
+
+    ahash = os.environ.get("API_HASH", "27e6993af0786f66f96599db6cd10bcc")
+
+    bot_token = os.environ.get("BOT_TOKEN", "5006835603:AAEOu5gqL7vi6CxnxLCo4lCgxh95aWFH2fU")
+
     sudo = [163494588, 1511373882]
-    # try:
-    #     sudo = set(int(x) for x in os.environ.get("SUDO", "").split(','))
-    # except ValueError:
-    #     raise Exception("Your sudo users list does not contain valid integers.")
-    
+
+
